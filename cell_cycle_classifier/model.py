e4ef76cb34ade45393ecd4dabc613bf169e88735
Adam Weiner <adamcweiner@gmail.com>
2020-10-18 21:06:23 -0400

    split ROC curves based on sample

--- cell_cycle_classifier/model.py
+++ cell_cycle_classifier/model.py
@@ -107,50 +107,54 @@ def train_test_model(
     training_data = feature_data.query('training_context == "training"')
     testing_data = feature_data.query('training_context == "holdout"')
     test_cell_ids = testing_data['cell_id'].values
+    testing_data['library_id'] = testing_data['cell_id'].apply(lambda x: x.split('-')[1])
+    testing_data['sample_id'] = testing_data['cell_id'].apply(lambda x: x.split('-')[0])
+    # sc_legend['cell_id'] = sc_legend['Sample'].apply(lambda x: x.split('.')[0])
 
     logging.info('training model')
     classifier = train_model(training_data, feature_names, random_state=random_seed)
 
-    X = testing_data[feature_names].values
-    y = testing_data['cell_cycle_state'].values == 'S'
-    # test_cell_ids = testing_data['cell_id'].values
+    for sample_id, chunk in testing_data.groupby('sample_id'):
+        X = chunk[feature_names].values
+        y = chunk['cell_cycle_state'].values == 'S'
 
-    logging.info(
-        'Accuracy of classifier on test set: {:.2f}'
-        .format(classifier.score(X, y)))
+        logging.info(
+            'Accuracy of classifier on test set: {:.2f}'
+            .format(classifier.score(X, y)))
 
-    y_pred = classifier.predict(X)
+        y_pred = classifier.predict(X)
 
-    logging.info("Accuracy: {}".format(metrics.accuracy_score(y, y_pred)))
-    logging.info("Precision: {}".format(metrics.precision_score(y, y_pred)))
+        logging.info("Accuracy: {}".format(metrics.accuracy_score(y, y_pred)))
+        logging.info("Precision: {}".format(metrics.precision_score(y, y_pred)))
 
-    y_pred_proba = classifier.predict_proba(X)[::,1]
-    fpr, tpr, _ = metrics.roc_curve(y, y_pred_proba)
+        y_pred_proba = classifier.predict_proba(X)[::,1]
+        fpr, tpr, _ = metrics.roc_curve(y, y_pred_proba)
 
-    if figures_prefix:
-        fig = plt.figure()
-        auc = metrics.roc_auc_score(y, y_pred_proba)
-        plt.plot(fpr,tpr,label="AUC={:.2f}, n={}".format(auc, y.shape[0]))
-        plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
-        plt.legend(loc=4)
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        seaborn.despine(offset=True, trim=True)
-        fig.savefig(figures_prefix + 'roc.pdf', bbox_inches='tight')
-    
-        fig = plt.figure()
-        feature_importance = pd.Series(dict(zip(feature_names, classifier.feature_importances_)))
-        feature_importance.plot.bar()
-        fig.savefig(figures_prefix + 'features.pdf', bbox_inches='tight')
+        if figures_prefix:
+            fig = plt.figure()
+            auc = metrics.roc_auc_score(y, y_pred_proba)
+            plt.plot(fpr,tpr,label="AUC={:.2f}, n={}".format(auc, y.shape[0]))
+            plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+            plt.legend(loc=4)
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
+            plt.title(sample_id)
+            seaborn.despine(offset=True, trim=True)
+            fig.savefig(figures_prefix + '_' + sample_id + 'roc.pdf', bbox_inches='tight')
+        
+            fig = plt.figure()
+            feature_importance = pd.Series(dict(zip(feature_names, classifier.feature_importances_)))
+            feature_importance.plot.bar()
+            fig.savefig(figures_prefix + 'features.pdf', bbox_inches='tight')
 
-    stats = dict(
-        accuracy=metrics.accuracy_score(y, y_pred),
-        precision=metrics.precision_score(y, y_pred),
-        recall=metrics.recall_score(y, y_pred),
-        f1=metrics.f1_score(y, y_pred),
-        auc=metrics.roc_auc_score(y, y_pred_proba),
-        random_seed=random_seed,
-    )
+        stats = dict(
+            accuracy=metrics.accuracy_score(y, y_pred),
+            precision=metrics.precision_score(y, y_pred),
+            recall=metrics.recall_score(y, y_pred),
+            f1=metrics.f1_score(y, y_pred),
+            auc=metrics.roc_auc_score(y, y_pred_proba),
+            random_seed=random_seed,
+        )
 
     return classifier, stats, y, y_pred, y_pred_proba, test_cell_ids
 

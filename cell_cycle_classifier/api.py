520ee017093cb959757e1f82520bba31472c8a31
Adam Weiner <adamcweiner@gmail.com>
2021-09-01 15:40:27 -0400

    only use RT features for human cells

--- cell_cycle_classifier/api.py
+++ cell_cycle_classifier/api.py
@@ -45,25 +45,72 @@ def train_classify(cn_data, metrics_data, align_metrics_data, figures_prefix=Non
         data['sample_id'] = data['cell_id'].apply(lambda a: a.split('-')[0])
         data['library_id'] = data['cell_id'].apply(lambda a: a.split('-')[1])
 
-    logging.info('calculating features')
+    # split data into human and non-human
+    metrics_data_nonhuman = metrics_data.query("species != 'grch37'")
+    metrics_data_human = metrics_data.query("species == 'grch37'")
+    nonhuman_cells = metrics_data_nonhuman.cell_id.unique()
+    align_metrics_data_nonhuman = align_metrics_data.loc[align_metrics_data['cell_id'].isin(nonhuman_cells)]
+    align_metrics_data_human = align_metrics_data.loc[~align_metrics_data['cell_id'].isin(nonhuman_cells)]
+    cn_data_nonhuman = cn_data.loc[cn_data['cell_id'].isin(nonhuman_cells)]
+    cn_data_human = cn_data.loc[~cn_data['cell_id'].isin(nonhuman_cells)]
 
+    # calculate features & make predictions for human cells
+    logging.info('calculating features for human cells')
     feature_data = features.calculate_features(
-        cn_data,
-        metrics_data,
-        align_metrics_data,
+        cn_data_human,
+        metrics_data_human,
+        align_metrics_data_human,
         figures_prefix=figures_prefix,
         use_rt_features=use_rt_features,
         use_pca_features=use_pca_features
     )
 
-    logging.info('predicting cell cycle')
-
+    logging.info('predicting cell cycle for human cells')
     predictions = model.predict(
         classifier,
         feature_data,
         use_rt_features=use_rt_features,
         use_pca_features=use_pca_features
     )
+    
+    # calculate features, train classifier, and predict state without RT or PCA features
+    # for the non-human cells
+    if len(nonhuman_cells) > 0:
+        logging.info('calculating features for non-human cells')
+        figures_prefix_nonhuman = figures_prefix + 'nonhuman_'
+
+        feature_data_nonhuman = features.calculate_features(
+            cn_data_nonhuman,
+            metrics_data_nonhuman,
+            align_metrics_data_nonhuman,
+            figures_prefix=figures_prefix_nonhuman,
+            use_rt_features=False,
+            use_pca_features=False
+        )
+
+        logging.info('training a classifier for non-human cells')
+        if use_curated_labels:
+            training_data_filename = pkg_resources.resource_filename('cell_cycle_classifier', 'data/training/curated_feature_data_v2.csv')
+        else:
+            training_data_filename = pkg_resources.resource_filename('cell_cycle_classifier', 'data/training/feature_data.csv.gz')
+        training_data_vanilla = pd.read_csv(training_data_filename)
+        classifier_vanilla, stats, __, __, __, __ = model.train_test_model(
+            training_data_vanilla,
+            figures_prefix=figures_prefix_nonhuman,
+            random_seed=42,
+            use_rt_features=False,
+            use_pca_features=False
+        )
+
+        logging.info('predicting cell cycle for non-human cells')
+        predictions_nonhuman = model.predict(
+            classifier_vanilla,
+            feature_data_nonhuman,
+            use_rt_features=False,
+            use_pca_features=False
+        )
+
+        predictions = pd.concat([predictions, predictions_nonhuman])
 
     predictions = predictions.merge(metrics_data[['cell_id']].drop_duplicates(), how='right')
     predictions['is_s_phase'] = predictions['is_s_phase'].fillna(False)
